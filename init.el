f979e8e0ce058a08b0a015bc6d40066483dddf6a
Jared Ross <jared.b.ross@gmail.com>
2018-03-19 11:18:12 +0200

    Updated lots

--- init.el
+++ init.el
@@ -26,6 +26,7 @@
  '(custom-safe-themes
    (quote
     ("ffe39e540469ef05808ab4b75055cc81266875fa4a0d9e89c2fec1da7a6354f3" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(js2-basic-offset 2)
  '(mac-option-modifier (quote meta))
  '(mac-right-option-modifier (quote hyper))
  '(nil nil t)
@@ -111,8 +112,13 @@
 
 	    ;; Fixing up certain states
 	    (evil-set-initial-state 'magit-mode 'emacs)
+	    (evil-set-initial-state 'magit-mode 'emacs)
 	    (evil-set-initial-state 'magit-popup-mode 'emacs)
-	    (evil-set-initial-state 'org-mode 'emacs))
+	    (evil-set-initial-state 'org-mode 'emacs)
+	    (evil-set-initial-state 'sql-interactive-mode 'emacs)
+	    (evil-set-initial-state 'neotree-mode 'emacs)
+
+	    (evil-mode 1))
   :ensure t)
 
 ;;; Rainbow Delimiters
@@ -177,6 +183,21 @@
 
 (use-package helm-dash
   :if 'helm
+  :ensure t)
+
+(use-package helm-ag
+  :ensure t)
+
+(use-package perspective
+  :ensure t)
+
+(use-package projectile
+  :ensure t)
+
+(use-package persp-projectile
+  :ensure t)
+
+(use-package helm-projectile
   :ensure t)
 
 (use-package bookmark+
@@ -352,6 +373,24 @@
   :config (progn
 	    (add-hook 'clojure-mode-hook #'subword-mode))
   :ensure t)
+
+(use-package rjsx-mode
+  :config (progn
+	    (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+  :ensure t
+  :pin melpa)
+
+(use-package neotree
+  :config (progn
+	      (global-set-key [f8] 'neotree-toggle))
+  :ensure t
+  :pin melpa)
+
+(use-package cider
+  :config (progn
+	    )
+  :ensure t
+  :pin melpa)
 
 (use-package cider
   :ensure t
