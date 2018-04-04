42f1939cd5f0d8eddfe563509c9ef0218873c47e
Jared Ross <jared.b.ross@gmail.com>
2018-04-04 23:16:44 +0300

    Fix Path, Reorganise main init.el

---
+++ init-sections/globals.el
@@ -0,0 +1,7 @@
+;;; Globals --- Working with global settings
+
+(use-package exec-path-from-shell
+  :config (progn
+	    (setq exec-path-from-shell-shell-name "sh")
+	    (exec-path-from-shell-initialize))
+  :ensure t)
--- init.el
+++ init.el
@@ -1,18 +1,33 @@
 ;;; Emacs Init file
 
-(load "~/.emacs.d/init-sections/init.el")
-(load "~/.emacs.d/init-sections/custom.el")
+;; Setting up the package management comes first.
 (load "~/.emacs.d/init-sections/package-management.el")
+
+;; Things that come before.
+(load "~/.emacs.d/init-sections/globals.el")
+(load "~/.emacs.d/init-sections/custom.el")
+
+;; Look and Feel.
 (load "~/.emacs.d/init-sections/theming.el")
+(load "~/.emacs.d/init-sections/emacs-comfort.el")
 (load "~/.emacs.d/init-sections/evil.el")
+
+;; Mix of things
 (load "~/.emacs.d/init-sections/misc-minor-modes.el")
+
+;; General programming
 (load "~/.emacs.d/init-sections/search-and-project-management.el")
 (load "~/.emacs.d/init-sections/syntax-management.el")
+
+;; Language Specific.
 (load "~/.emacs.d/init-sections/latex.el")
 (load "~/.emacs.d/init-sections/org.el")
 (load "~/.emacs.d/init-sections/programming-modes.el")
+
+;; Safety Features
 (load "~/.emacs.d/init-sections/emacs-safety.el")
-(load "~/.emacs.d/init-sections/emacs-comfort.el")
-(load "~/.emacs.d/init-sections/osx.el")
+
+(if (eq system-type 'darwin)
+    (load "~/.emacs.d/init-sections/osx.el"))
 
 (provide 'init)
