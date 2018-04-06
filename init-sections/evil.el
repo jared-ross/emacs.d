73320fbf5da121760ec438efc81ba5309539fc7e
Jared Ross <jared.b.ross@gmail.com>
2018-04-06 23:34:16 +0300

    Evil: Add surround, commentary, exchange

--- init-sections/evil.el
+++ init-sections/evil.el
@@ -39,4 +39,60 @@
 	    (evil-set-initial-state 'sql-interactive-mode 'emacs)
 	    (evil-set-initial-state 'neotree-mode 'emacs)
 
+            ;; Turning on Evil mode
 	    (evil-mode 1)))
+
+(use-package evil-surround
+  :config (global-evil-surround-mode 1))
+
+;; Evil Unimpared, stolen from Sylvain Benner <sylvain.benner@gmail.com>
+;; https://github.com/zmaas/evil-unimpaired/blob/master/evil-unimpaired.el
+
+(use-package dash)
+(use-package f)
+
+(defun evil-unimpaired/paste-above ()
+  (interactive)
+  (evil-insert-newline-above)
+  (evil-paste-after 1))
+
+(defun evil-unimpaired/paste-below ()
+  (interactive)
+  (evil-insert-newline-below)
+  (evil-paste-after 1))
+
+(defun evil-unimpaired/insert-space-above (count)
+  (interactive "p")
+  (dotimes (_ count) (save-excursion (evil-insert-newline-above))))
+
+(defun evil-unimpaired/insert-space-below (count)
+  (interactive "p")
+  (dotimes (_ count) (save-excursion (evil-insert-newline-below))))
+
+(define-key evil-normal-state-map (kbd "[ SPC") 'evil-unimpaired/insert-space-above)
+(define-key evil-normal-state-map (kbd "] SPC") 'evil-unimpaired/insert-space-below)
+
+(when (featurep 'flycheck)
+	(define-key evil-normal-state-map (kbd "] l") 'flycheck-next-error)
+	(define-key evil-normal-state-map (kbd "[ l") 'flycheck-previous-error)
+	(define-key evil-normal-state-map (kbd "] q") 'flycheck-next-error)
+	(define-key evil-normal-state-map (kbd "[ q") 'flycheck-previous-error))
+
+;; (define-key evil-normal-state-map (kbd "[ e") 'move-text-up)
+;; (define-key evil-normal-state-map (kbd "] e") 'move-text-down)
+;; (define-key evil-visual-state-map (kbd "[ e") ":move'<--1")
+;; (define-key evil-visual-state-map (kbd "] e") ":move'>+1")
+
+;; select pasted text
+(define-key evil-normal-state-map (kbd "g p") (kbd "` [ v ` ]"))
+
+;; paste above or below with newline
+(define-key evil-normal-state-map (kbd "[ p") 'evil-unimpaired/paste-above)
+(define-key evil-normal-state-map (kbd "] p") 'evil-unimpaired/paste-below)
+
+
+(use-package evil-commentary
+  :config (evil-commentary-mode))
+
+(use-package evil-exchange
+  :config (evil-exchange-cx-install))
