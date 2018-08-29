683100b98b2e743ff0718fd8e1444aa60e46a16b
Jared Ross <jared.b.ross@gmail.com>
2018-08-29 20:04:25 +0800

    Turn of rjsx flycheck?

--- init-sections/syntax-management.el
+++ init-sections/syntax-management.el
@@ -1,32 +1,38 @@
 ;;; Syntax Management
 
 ;; Fly Check
-;; This doesn't work when first loaded but when I reload it it works
-(use-package flycheck
-  :config (progn
-	    (flycheck-define-checker javascript-eslint
-	      "A Javascript syntax and style checker using eslint.
+
+;; Hack to get eslint to work properly
+(defun jared-syntax-install-eslint-checker ()
+  (interactive)
+  (flycheck-define-checker javascript-eslint
+    "A Javascript syntax and style checker using eslint.
 
 See URL `http://eslint.org/'."
-	      :command ("npx" "eslint" "--format=json"
-			(option-list "--rulesdir" flycheck-eslint-rules-directories)
-			"--stdin" "--stdin-filename" source-original)
-	      :standard-input t
-	      :error-parser flycheck-parse-eslint
-	      ; Hack: fix: (flycheck-eslint-config-exists-p)
-	      :enabled (lambda () (quote t))
-	      :modes (js-mode js-jsx-mode js2-mode js2-jsx-mode js3-mode rjsx-mode)
-	      :working-directory flycheck-eslint--find-working-directory
-	      :verify
-	      (lambda (_)
-		(let* ((default-directory
-			 (flycheck-compute-working-directory 'javascript-eslint))
-		       (have-config t))
-		  (list
-		   (flycheck-verification-result-new
-		    :label "config file"
-		    :message (if have-config "found" "missing or incorrect")
-		    :face (if have-config 'success '(bold error)))))))
+    :command ("npx" "eslint" "--format=json"
+              (option-list "--rulesdir" flycheck-eslint-rules-directories)
+              "--stdin" "--stdin-filename" source-original)
+    :standard-input t
+    :error-parser flycheck-parse-eslint
+                                        ; Hack: fix: (flycheck-eslint-config-exists-p)
+    :enabled nil
+    :modes (js-mode js-jsx-mode js2-mode js2-jsx-mode js3-mode rjsx-mode)
+    :working-directory flycheck-eslint--find-working-directory
+    :verify
+    (lambda (_)
+      (let* ((default-directory
+               (flycheck-compute-working-directory 'javascript-eslint))
+             (have-config t))
+        (list
+         (flycheck-verification-result-new
+          :label "config file"
+          :message (if have-config "found" "missing or incorrect")
+          :face (if have-config 'success '(bold error))))))))
+
+(use-package flycheck
+  :config (progn
+            ;; (jared-syntax-install-eslint-checker)
+            ;; (add-hook 'rjsx-mode #'jared-syntax-install-eslint-checker)
             (add-hook 'prog-mode-hook #'flycheck-mode)))
 
 ;;; Hippie Expand
