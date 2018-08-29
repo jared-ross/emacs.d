76c85beb92d847bc73eb724724425ed6793d9fb5
Jared Ross <jared.b.ross@gmail.com>
2018-08-29 20:03:17 +0800

    Add commented out grayscale theme, to watch out for

--- init-sections/theming.el
+++ init-sections/theming.el
@@ -1,5 +1,13 @@
 ;;; Theming
 
+;; Old Theme
 (use-package leuven-theme
   :config (progn
-	    (load-theme 'leuven t t)))
+	    (load-theme 'leuven t t)
+            (set-face-foreground 'mode-line "RosyBrown")
+            (set-face-foreground 'mode-line-inactive "#DDD")
+            ))
+
+;; Experimental New Theme
+;; (use-package grayscale-theme
+;;   :ensure t)
