452d9d7d27ce8dc117d77dfb7913d9dae86ff1ee
Jared Ross <jared.b.ross@gmail.com>
2018-08-29 15:46:17 +0800

    Fix elisp doc check

--- init-sections/custom.el
+++ init-sections/custom.el
@@ -11,9 +11,13 @@
  '(custom-safe-themes
    (quote
     ("ffe39e540469ef05808ab4b75055cc81266875fa4a0d9e89c2fec1da7a6354f3" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(package-selected-packages
+   (quote
+    (polymode tide yaml-mode php-mode dired+ ace-window window-purpose bind-key yafolding writeroom-mode workgroups2 whitespace-cleanup-mode use-package tea-time sublimity spray solarized-theme smooth-scroll smart-mode-line sicp sclang-extensions rjsx-mode redtick rainbow-delimiters racket-mode powerline pomodoro pomidor persp-projectile persp-mode origami org-plus-contrib org neotree names magit leuven-theme key-chord json-mode helm-projectile helm-dash haskell-mode guide-key golden-ratio gnugo flycheck flx-ido exec-path-from-shell evil-surround evil-numbers evil-leader evil-exchange evil-commentary eterm-256color diminish darkroom counsel color-theme-solarized clj-refactor chess cdlatex bookmark+ bongo aurora-theme auctex ample-theme aggressive-indent ag add-node-modules-path 4clojure)))
  '(safe-local-variable-values
    (quote
-    ((flycheck-disabled-checkers emacs-lisp-checkdoc)
+    ((eval org-content 2)
+     (flycheck-disabled-checkers emacs-lisp-checkdoc)
      (flycheck-disabled-checkers . emacs-lisp-checkdoc)))))
 
 
